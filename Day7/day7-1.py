39f5f3042edac05f9ce0d6ccc5cd9e05c78e5c96
Adam Iannazzone <adam.iannazzone@gmail.com>
2020-12-07 12:31:12 -0500

    Update day7-1.py
    Changing from dictionary to object approach. This is a tough one!

--- Day7/day7-1.py
+++ Day7/day7-1.py
@@ -7,44 +7,33 @@ def cleanInput(x):
 	x = list(map(lambda st: str.replace(st, '  ', ' '), x))
 	x = list(map(lambda st: str.replace(st, ' ,', ','), x))
 	return x
-def createDict(x):
+def createBag(x):
 	thisRule = x.strip().split(' contain ')
 	thisRule[1] = thisRule[1].split(', ')
-
+	thisColor = thisRule[0]
 	thisContents = []
 	if thisRule[1][0] != 'no other':
 		for i in thisRule[1]:
 			thisContents.append((i[2:], i[0]))
-	thisDict = {
-		'color': thisRule[0],
-		'contents': thisContents
-	}
-	return thisDict
+	allRules.append(Bag(thisColor, thisContents))
 def checkBag(x):
 	for i in range(len(x)):
 		if x[i][0] == 'shiny gold':
 			return True
 		elif x[i] != []:
-			pprint("Check These: " + str(x[i][0]))
+			return False
 		else:
 			return False
+class Bag:
+	def __init__(thisBag, color, contents):
+		thisBag.color = color
+		thisBag.contents = contents
 
 # Open the file and clean up the text
 rawRules = open('sample.txt', 'r').read().splitlines()
 rawRules = cleanInput(rawRules)
+# pprint(rawRules[0])
 
-# Generate list of rules where each
-# Rule is a dictionary entry
 allRules = []
 for i in rawRules:
-	allRules.append(createDict(i))
-
-shinyGoldCount = 0
-notEmptyCount = 0
-pprint(allRules)
-
-for i in range(len(allRules)):
-	if checkBag(allRules[i]['contents']):
-		shinyGoldCount += 1
-
-print("Shiny Gold Rules: " + str(shinyGoldCount))
+	createBag(i)
