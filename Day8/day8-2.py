9244818efeb071bdc30af66ae3fa2663305aa4f4
Adam Iannazzone <adam.iannazzone@gmail.com>
2020-12-08 12:13:34 -0500

    8-2 update

--- Day8/day8-2.py
+++ Day8/day8-2.py
@@ -1,42 +1,45 @@
 from pprint import pprint
 
+def checkChange(instructions):
+	j = 0
+	alreadyRun = []
+	accumulator = 0
+	while True:
+		thisInstruction = instructions[j][0]
+		thisValue = instructions[j][1]
+		alreadyRun.append(j)
+
+		# Run the instructions
+		if thisInstruction == 'acc':
+			accumulator += thisValue
+			j += 1
+		elif thisInstruction == 'jmp':
+			j += thisValue
+		else:
+			j += 1
+
+		# Check to see if we fixed the boot sequence
+		if j in alreadyRun:
+			break
+		elif j == len(instructions):
+			print(accumulator)
+			break
+
+# Open and clean up input
 rawInstructions = open('sample.txt', 'r').read().splitlines()
-instructions = []
+instructions = [] #each line becomes an item in this list
 for i in rawInstructions:
 	a = str(i[:3])
 	b = int(i[4:])
-	instructions.append([a, b])
+	instructions.append([a, b]) #each instruction is a list w/2 items
 
-accumulator = 0
-j = 0
-alreadyRun = []
+for i in range(len(instructions)):
+	newInstructions = instructions.copy()
+	pprint(newInstructions)
+	thisInstruction = newInstructions[i][0]
+	# print(str(i) + ': ' + thisInstruction)
 
-while j < len(instructions):
-	thisInstruction = instructions[j][0]
-	thisValue = instructions[j][1]
-	if j in alreadyRun:
-		if instructions[j][0] == 'nop':
-			instructions[j][0] = 'jmp'
-		else:
-			instructions[j][0] = 'nop'
-	alreadyRun.append(j)
-	if thisInstruction == 'acc':
-		accumulator += thisValue
-		j += 1
+	if thisInstruction == 'nop':
+		newInstructions[i][0] = 'jmp'
 	elif thisInstruction == 'jmp':
-		j += thisValue
-	else:
-		j += 1
-
-j = 0
-while j < len(instructions):
-	thisInstruction = instructions[j][0]
-	thisValue = instructions[j][1]
-	if thisInstruction == 'acc':
-		accumulator += thisValue
-		j += 1
-	elif thisInstruction == 'jmp':
-		j += thisValue
-	else:
-		j += 1
-print(accumulator)
+		newInstructions[i][0] = 'nop'
