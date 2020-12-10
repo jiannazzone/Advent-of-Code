347a2a504716ec310363fa2269cb32a6af36bf2c
Adam Iannazzone <adam.iannazzone@gmail.com>
2020-12-10 10:33:56 -0500

    Update day10-1.py
    Completed task 1

--- Day10/day10-1.py
+++ Day10/day10-1.py
@@ -1,1 +1,26 @@
 from pprint import pprint
+
+# Import adapter data and generate myDevice rating
+allAdapters = open('input.txt', 'r').read().splitlines()
+allAdapters = [int(x) for x in allAdapters]
+myDevice = max(allAdapters) + 3
+
+# Add data for outlet and my device
+allAdapters.append(myDevice)
+allAdapters.append(0)
+
+sortedAdapters = sorted(allAdapters)
+pprint('All Adapters: ' + str(allAdapters))
+pprint('Sorted Adapters: ' + str(sortedAdapters))
+pprint('My Device: ' + str(myDevice))
+
+single_jolt = 0
+triple_jolt = 0
+for i in range(1, len(sortedAdapters)):
+	delta = sortedAdapters[i] - sortedAdapters[i-1]
+	if delta == 1:
+		single_jolt += 1
+	elif delta == 3:
+		triple_jolt += 1
+pprint('Single Jolts: ' + str(single_jolt))
+pprint('Triple Jolts: ' + str(triple_jolt))
