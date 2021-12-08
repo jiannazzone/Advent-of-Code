e9397adea36dd5ab3cd843acdfb15814f2ccfbed
Adam Iannazzone <adam@iannaz.zone>
2021-12-08 12:01:42 -0500

    Update main.py
    - Part 2 solved

--- Day08/main.py
+++ Day08/main.py
@@ -1,5 +1,6 @@
 # Day 08
 from pprint import pprint
+from typing import final
 
 # Dictionary with all of the numbers and their corresponding segments
 # The number 8 would be rendered using the following segments
@@ -67,6 +68,8 @@ def part1(data):
 
 def part2(data):
 
+    sum = 0
+
     for entry in data:
         # Prepare an empty dictionary to store the number mappings for this entry
         thisMapping = {}
@@ -103,7 +106,7 @@ def part2(data):
                 thisSegmentMapping['a'] = letter
                 break
         
-        # Make lists of the signals with 5 letters and 6 letters for comparisons
+        # Make lists of the signals with 5 letters and 6 letters for analysis
         fiveLetterNumbers = []
         sixLetterNumbers = []
         for signal in entry[0]:
@@ -115,12 +118,10 @@ def part2(data):
         # Compare all of the 5-letter signals
         # For the three signals, they all share three letters. Those are the a, d and g letters
         # Then see which of those is present in 4... that is your d. The other is g
-        
         commonLetters = ''
         for letter in fiveLetterNumbers[0]:
             if letter in fiveLetterNumbers[1] and letter in fiveLetterNumbers[2]:
                 commonLetters += letter
-
         for letter in commonLetters:
             if letter != thisSegmentMapping['a']:
                 if letter in thisMapping[4]:
@@ -133,19 +134,70 @@ def part2(data):
         horizontalLetters = [thisSegmentMapping['a'],thisSegmentMapping['d'], thisSegmentMapping['g']]
         for signal in sixLetterNumbers:
             if not all((x in signal) for x in horizontalLetters):
-                thisMapping[0] = signal
+                thisMapping[0] = signal # Map signal to 0 in our dictionary
+                sixLetterNumbers.remove(signal) # Remove 0 from the remaining 6-letter numbers
+        
+        # Look at the signals for 6 and 9 (we don't know which is which, yet)
+        # They are both missing one letter
+        # If that *missing* letter is present in the signal for 4, then it's a 6. Otherwise, it's a 9
+        for signal in sixLetterNumbers:
+            missingLetter = ''
+            for letter in 'abcdefg':
+                if letter not in signal:
+                    missingLetter = letter
+            if missingLetter in thisMapping[4]:
+                thisSegmentMapping['c'] = missingLetter
+                thisMapping[6] = signal
+            else:
+                thisSegmentMapping['e'] = missingLetter
+                thisMapping[9] = signal
 
-        # Function output
-        print('Letter mappings: ')
-        print(thisSegmentMapping)
-        print('----------')
-        print(f'Solved numbers:')
-        print(thisMapping)
+        # Look at the 5-letter signals
+        # The signal for 5 is the same as 9, but with the "c" position removed
+        # Turn a 9 signal into a 5 signal and compare to figure out the signal for 5
+        for signal in fiveLetterNumbers:
+            modifiedNine = thisMapping[9].replace(thisSegmentMapping['c'],'')
+            if ''.join(sorted(signal)) == ''.join(sorted(modifiedNine)):
+                thisMapping[5] = signal
+                fiveLetterNumbers.remove(signal)
+        
+        # The 2-signal has two differences with the 5-signal
+        # Use this to identify the signal for 2, then the 3
+        for signal in fiveLetterNumbers:
+            differences = 0
+            for letter in signal:
+                if letter not in thisMapping[5]:
+                    differences += 1
+            if differences == 2:
+                thisMapping[2] = signal
+                fiveLetterNumbers.remove(signal)
+                thisMapping[3] = fiveLetterNumbers[0]
+
+        # Alphabetize the values in thisMapping
+        for key in thisMapping.keys():
+            thisMapping[key] = ''.join(sorted(thisMapping[key]))
+        
+        # Decode the output signal
+        finalOutput = ''
+        for signal in entry[1]:
+            for key in thisMapping.keys():
+                if thisMapping[key] == ''.join(sorted(signal)):
+                    finalOutput += str(key)
+                    break
+        
+        sum += int(finalOutput)
+    
+    # Function output
+    print('----------')
+    print(f'This Mapping Sorted:')
+    print(thisMapping)
+    print('----------')
+    print(f'Final Sum: {sum}')
 
 
-filepath = 'Day08/sample1.txt'
+# filepath = 'Day08/sample1.txt'
 # filepath = 'Day08/sample2.txt'
-# filepath = 'Day08/input.txt'
+filepath = 'Day08/input.txt'
 
 data = prepareData(filepath)
 # part1(data)
