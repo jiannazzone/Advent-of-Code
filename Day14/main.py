3c895ad52fabc6195e256a87e0a5ab0e3b2994d6
Adam Iannazzone <adam@iannaz.zone>
2021-12-14 09:03:27 -0500

    Day 14, Part 1 complete

--- Day14/main.py
+++ Day14/main.py
@@ -11,20 +11,59 @@ def prepareData(isSample):
         data = f.read()
 
     polymerTemplate = data.split('\n\n')[0]
-    insertionRules = data.split('\n')[2:]
 
-    # Reformat insertion rules as tuples, where
-    # AB -> C become ('AB', 'C')
-    for i in range(len(insertionRules)):
-        thisRule = insertionRules[i].split(' -> ')
-        insertionRules[i] = (thisRule[0], thisRule[1])
+    # Create dictionary with all insertion rules
+    # AB -> C becomes the key/val pair 'AB': 'C'
+    insertionDictionary = {}
+    for rule in data.split('\n')[2:]:
+        thisRule = rule.split(' -> ')
+        insertionDictionary[thisRule[0]] = thisRule[1]
 
-    return polymerTemplate, insertionRules
-
-def part1(isSample):
-    polymerTemplate, insertionRules = prepareData(isSample)
-
+    return polymerTemplate, insertionDictionary
 
 # --- MAIN CODE --- #
 isSample = True
-part1(isSample)
+numSteps = 10
+
+polymerString, insertionDictionary = prepareData(isSample)
+# print(f'Template: {polymerString}')
+
+# Each part will be examined against the insertion rules
+# If a pair matches a rule, a tuple will be generated with the format (position, letter)
+# For example: NNCB with an insertion rule of NN -> C will generate a tuple (1, 'C')
+
+# Iterate for a specified number of steps
+for step in range(numSteps):
+    newPolymerString = ''
+
+    # Look through the entire polymer string
+    for i in range(len(polymerString) - 1):
+        thisLookup = polymerString[i:i+2]
+
+        # Generate insertion if the pair exists in the insertion dictionary
+        if thisLookup in insertionDictionary:
+            newString = f'{polymerString[i]}{insertionDictionary[thisLookup]}'
+            newPolymerString += newString
+    
+    newPolymerString += polymerString[-1]
+    polymerString = newPolymerString
+
+    # print(f'After step {step+1}: {polymerString}')
+
+# Create dictionary to store letter frequencies
+letterFrequency = {}
+for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    letterFrequency[letter] = 0
+
+# Calculate letter frequencies
+for letter in polymerString:
+    letterFrequency[letter] += 1
+
+# Find min and max letter frequencies
+letterFrequencyList = sorted(letterFrequency.values(), reverse=True)
+maxLetterFrequency = letterFrequencyList[0]
+minLetterFrequency = letterFrequencyList[letterFrequencyList.index(0) - 1]
+print(f'Max Letter Frequency: {maxLetterFrequency}')
+print(f'Min Letter Frequency: {minLetterFrequency}')
+
+print(f'Frequency Spread: {maxLetterFrequency - minLetterFrequency}')
