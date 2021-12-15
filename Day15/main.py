b7847b5aa1f7652f321407bdaa2465b0e693862b
Adam Iannazzone <adam@iannaz.zone>
2021-12-15 09:20:17 -0500

    Part 2 Complete

--- Day15/main.py
+++ Day15/main.py
@@ -1,9 +1,10 @@
 # Day 15
 from pprint import pprint
+import time
 
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
-from pathfinding.finder.a_star import AStarFinder
+from pathfinding.finder.dijkstra import DijkstraFinder
 
 
 def prepareData(isSample):
@@ -31,7 +32,7 @@ def part1(isSample):
     start = grid.node(0, 0)
     end = grid.node(len(data)-1, len(data[0])-1)
 
-    finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+    finder = DijkstraFinder(diagonal_movement=DiagonalMovement.never)
     path, runs = finder.find_path(start, end, grid)
 
     # Get sum of all values along path, subtracting the initial position
@@ -51,22 +52,76 @@ def part2(isSample):
     # The left shows how values change each iteration
     # The right shows which grids are identical to each other
     # # # # # # # -- # # # # # # #
-    # 8 9 1 2 3 # -- # 1 2 3 4 5 #
-    # 9 1 2 3 4 # -- # 2 3 4 5 6 #
-    # 1 2 3 4 5 # -- # 3 4 5 6 7 #
-    # 2 3 4 5 6 # -- # 4 5 6 7 8 #
-    # 3 4 5 6 7 # -- # 5 6 7 8 9 #
-    # # # # # # # -- # # # # # # #
+    # 8 9 1 2 3 # -- # 0 1 2 3 4 #
+    # 9 1 2 3 4 # -- # 1 2 3 4 5 #
+    # 1 2 3 4 5 # -- # 2 3 4 5 6 #
+    # 2 3 4 5 6 # -- # 3 4 5 6 7 #
+    # 3 4 5 6 7 # -- # 4 5 6 7 8 #
+    # # # # # # # -- # # # # # # # 
 
     # Create 9 iterations of the grid. The first iteration is just the original grid
     allIterations = [startingGrid]
-
-    with open('Day15/sampleGiantGrid.txt') as f:
-        sampleGrid = [[int(x) for x in row] for row in f.read().split('\n')]
     
-    # if sampleGrid == newGrid:
-    #     print('Huzzah!')
+    # We need 8 more iterations of the grid
+    lastIteration = startingGrid
+    for i in range(8):
+        thisIteration = []
+        for row in lastIteration:
+            newRow = []
+            for num in row:
+                if num == 9:
+                    newRow.append(1)
+                else:
+                    newRow.append(num + 1)
+            thisIteration.append(newRow)
+        allIterations.append(thisIteration)
+        lastIteration = thisIteration
+    
+    # Now that we have all the ierations, it's just a matter of concatenating them appropriately
+    finalGrid = []
+    
+    for i in range(5):
+        # Iterate across each 'meta-row' corresponding to one row of concatenated grids
+        for row in range(len(allIterations[0])):
+            thisRow = []
+            for j in range(5):
+                rowToAdd = allIterations[i + j][row]
+                for num in rowToAdd:
+                    thisRow.append(num)
+            finalGrid.append(thisRow)
+    
+    print('Final Grid Assembled; now pathfinding. This could take a while...')
+    print(f'Your grid is {len(finalGrid)} x {len(finalGrid[0])}')
 
-isSample = True
-# part1(isSample)
+    # -- Debugging -- #
+    # with open('Day15/sampleGiantGrid.txt') as f:
+    #     sampleGrid = [[int(x) for x in row] for row in f.read().split('\n')]
+    
+    # if sampleGrid == finalGrid:
+    #     print('Huzzah!')
+    # -- Debugging -- #
+
+    # Now we just need to pathfind! Copy code from Part 1
+    grid = Grid(matrix=finalGrid)
+
+    start = grid.node(0, 0)
+    end = grid.node(len(finalGrid)-1, len(finalGrid[0])-1)
+
+    finder = DijkstraFinder(diagonal_movement=DiagonalMovement.never)
+    path, runs = finder.find_path(start, end, grid)
+
+    # Get sum of all values along path, subtracting the initial position
+    sum = 0
+    for coordinate in path:
+        sum += finalGrid[coordinate[1]][coordinate[0]]
+    sum -= finalGrid[0][0]
+    
+    print(f'Path Sum: {sum}')
+
+start_time = time.time()
+isSample = False
+print('\n--- Part 1 ---')
+part1(isSample)
+
+print('\n--- Part 2 ---')
 part2(isSample)
