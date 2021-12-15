aead920898201d5dbb5af3e272fb1610d96a6f29
Adam Iannazzone <adam@iannaz.zone>
2021-12-15 14:10:09 -0500

    Day 10, Part 2 Complete

--- Day10/main.py
+++ Day10/main.py
@@ -17,48 +17,69 @@ def checkScore(line, points):
         if character in points:
             return points[character]
 
-def part1(isSample):
-    data = prepareData(isSample)
-    pairs = ['()', '[]', '{}', '<>']
-    openingTags = ['(', '[', '{', '<']
-    closingTags = [')', ']', '}', '>']
-    points = {
-        ')': 3,
-        ']': 57,
-        '}': 1197,
-        '>': 25137
-    }
-    
-    mismatches = []
-    for i in range(len(openingTags)):
-        for j in range(len(closingTags)):
-            if i != j:
-                mismatches.append(openingTags[i] + closingTags[j])
-
-    # Loop through each line of data and reduce until no more changes are being applied
-    while True:
-        p = re.compile('|'.join(map(re.escape, pairs)))
-        reducedData = [p.sub('', s) for s in data]
-        if reducedData == data:
-            break
-        else:
-            data = reducedData
-    
-    # Once reduced look for mismatched pairs
-    # Generate an index os corrupted lines
-    corruptedLines = []
-    for i in range(len(data)):
-        for mismatch in mismatches:
-            if mismatch in data[i]:
-                corruptedLines.append(data[i])
-    
-    pprint(corruptedLines)
-    
-    # Get the score
+def checkScore2(line, points):
     score = 0
-    for line in corruptedLines:
-        score += checkScore(line, points)
-    print(f'Final score: {score}')
+    for char in line[-1::-1]:
+        score *= 5
+        score += points[char]
+    return score
 
 isSample = False
-part1(isSample)
+data = prepareData(isSample)
+pairs = ['()', '[]', '{}', '<>']
+openingTags = ['(', '[', '{', '<']
+closingTags = [')', ']', '}', '>']
+incompletePoints = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+completePoints = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4
+}
+
+mismatches = []
+for i in range(len(openingTags)):
+    for j in range(len(closingTags)):
+        if i != j:
+            mismatches.append(openingTags[i] + closingTags[j])
+
+# Loop through each line of data and reduce until no more changes are being applied
+while True:
+    p = re.compile('|'.join(map(re.escape, pairs)))
+    reducedData = [p.sub('', s) for s in data]
+    if reducedData == data:
+        break
+    else:
+        data = reducedData
+
+# Once reduced look for mismatched pairs
+# Generate a list of corrupted lines
+corruptedLines = []
+for i in range(len(data)):
+    for mismatch in mismatches:
+        if mismatch in data[i]:
+            corruptedLines.append(data[i])
+
+# Get the score for incomplete 
+score = 0
+for line in corruptedLines:
+    score += checkScore(line, incompletePoints)
+print(f'Part 1 Score: {score}')
+
+# Get a list of the incomplete lines
+incompleteLines = []
+for line in data:
+    if line not in corruptedLines:
+        incompleteLines.append(line)
+
+# Calculate the scores for each incomplete line
+scores = [checkScore2(line, completePoints) for line in incompleteLines]
+winningScore = sorted(scores)[len(scores)//2]
+print('Part 2 Scores:')
+pprint(scores)
+print(f'Winning Score: {winningScore}')
