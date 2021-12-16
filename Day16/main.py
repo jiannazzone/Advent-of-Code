8e5f174f1bcc8280d3d41044e0635ba4857ff3ad
Adam Iannazzone <adam@iannaz.zone>
2021-12-16 08:31:10 -0500

    Update main.py
    - Parse binary string for version, type, and len(5) substrings
    - Parse literal strings

--- Day16/main.py
+++ Day16/main.py
@@ -1,8 +1,7 @@
 # Day 16
 from pprint import pprint
 
-def hexToBinary(hexString):
-    hexToBinaryDict = {
+hexToBinaryDict = {
         '0': '0000',
         '1': '0001',
         '2': '0010',
@@ -19,10 +18,43 @@ def hexToBinary(hexString):
         'D': '1101',
         'E': '1110',
         'F': '1111'
-    }
+}
+binaryToHexDict = {val: key for key, val in hexToBinaryDict.items()}
+
+def hexToBinary(hexString):
     binaryString = ''
     for char in hexString:
         binaryString += hexToBinaryDict[char]
     return binaryString
 
-binaryString = hexToBinary('38006F45291200')
+def parseBinaryString(binaryString):
+    # This will extract the version and type of the packet
+    # Version, type, and the remainder will all be returned
+
+    bitsVersion = binaryString[:3]
+    bitsType = binaryString[3:6]
+    dataString = binaryString[6:]
+
+    print(f'Version: {bitsVersion}')
+    print(f'Type: {bitsType}')
+    pprint(dataString)
+
+    return bitsVersion, bitsType, dataString
+
+def parseLiteralString(dataString):
+    # Break literal strings into groups of 5 for analysis
+
+    literalString = ''
+    bitsStrings = [dataString[i:i+5] for i in range(0, len(dataString), 5)]
+    print(bitsStrings)
+    for substring in bitsStrings:
+        literalString += substring[1:]
+        if substring[0] == '0':
+            return int(literalString, 2)
+
+binaryString = hexToBinary('D2FE28')
+bitsVersion, bitsType, dataString = parseBinaryString(binaryString)
+
+if bitsType == '100':
+    literalValue = parseLiteralString(dataString)
+    print(f'Literal String: {literalValue}')
