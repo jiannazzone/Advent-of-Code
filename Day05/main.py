d7a9800d1ba0e2349fdf0665f720eaf516ec03f9
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-03-02 09:01:07 -0500

    Day 5, Part 2 complete

--- Day05/main.py
+++ Day05/main.py
@@ -6,6 +6,7 @@ with open(filename) as f:
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 
+# Part 1 Functions
 def checkVowels(line):
     vowelCount = 0
     for letter in line:
@@ -36,11 +37,38 @@ def checkForDouble(line):
     # If we reached this point, there are no double letters
     return False
 
-def part1() :
+def part1():
     niceLines = []
     for line in data:
         if checkIllegal(line) and checkForDouble(line) and checkVowels(line):
             niceLines.append(line)
-    print(f'This data set has {len(niceLines)} nice lines.')
+    print(f'\n-----Part 1-----\nThis data set has {len(niceLines)} nice lines.\n')
 
+# Part 2 Functions
+def checkForXYX(line):
+    for i in range(len(line)-2):
+        if line[i] == line[i+2]:
+            return True
+    # If we exited the for-loop, then there are no XYX patterns
+    return False
+
+def checkForRepeatDoubles(line):
+    pairs = []
+    for i in range(len(line)-1):
+        pairs.append(f'{line[i]}{line[i+1]}')
+    for i in range(len(pairs)-1):
+        if pairs[i] in pairs[i+2:]:
+            return True
+    # If we reached this point, there are no pairs 
+    return False
+
+def part2():
+    niceLines = []
+    for line in data:
+        if checkForRepeatDoubles(line) and checkForXYX(line):
+            niceLines.append(line)
+    print(f'\n-----Part 2-----\nThis data set has {len(niceLines)} nice lines.\n')
+
+# Main Code
 part1()
+part2()
