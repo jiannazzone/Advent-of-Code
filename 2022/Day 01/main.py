d6dc5933e89125dbd7e948323d83a829c3daca55
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-01 07:45:34 -0500

    Part 1 complete

--- 2022/Day 01/main.py
+++ 2022/Day 01/main.py
@@ -13,7 +13,24 @@ def processInput(filename):
                 thisElf.append(int(y))
             calorieCount.append(thisElf)
 
-        print(calorieCount)
+        return calorieCount
 
-part1Sample = 'Day 01/sample1.txt'
-processInput(part1Sample)
+def findMostCalories(calorieCount):
+    maxCalorieIndex = 0
+    maxCalorieCount = 0
+
+    # Iterate through each elf and check their calorie sum
+    # If the sum is greater than the previous max, update values
+    for i in range(len(calorieCount)):
+        thisElfCalories = sum(calorieCount[i])
+        if thisElfCalories > maxCalorieCount:
+            maxCalorieCount = thisElfCalories
+            maxCalorieIndex = i
+    
+    # Now we have iterated through all of the elves and can print out the results
+    print(f'The most over-prepared elf is #{maxCalorieIndex + 1} with {maxCalorieCount} calories.')
+
+
+part1Sample = 'Day 01/input1.txt'
+calorieCount = processInput(part1Sample)
+findMostCalories(calorieCount)
