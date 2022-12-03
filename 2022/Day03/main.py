cdde5a0ef98797087b5f33a44671d25a616854db
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-03 11:04:22 -0500

    Part 1 complete

--- 2022/Day03/main.py
+++ 2022/Day03/main.py
@@ -1,12 +1,34 @@
 #  Day 3
+import string
 
 def processInput(filename):
     with open(filename) as f:
         data = f.read().split('\n')
     rucksacks = [ (x[:len(x)//2], x[len(x)//2:]) for x in data]
-    return data
+    return rucksacks
     
-
-filename = 'Day03/sample.txt'
+filename = 'Day03/input.txt'
 rucksacks = processInput(filename)
-print(rucksacks)
+priorityList = list(string.ascii_letters)
+
+
+# Part 1
+def part1(rucksacks):
+    # Determine which letters are in common for each compartment of each rucksack
+    lettersInCommon = []
+    for r in rucksacks:
+        lettersInCommon.append(findLetterInCommon(r))
+    # print(lettersInCommon)
+
+    # Get priority of each letter and sum
+    prioritySum = 0
+    for letter in lettersInCommon:
+        prioritySum += priorityList.index(letter) + 1
+    print(f'The priority sum is {prioritySum}')
+
+def findLetterInCommon(rucksack):
+    for letter in rucksack[0]:
+        if letter in rucksack[1]:
+            return letter    
+
+part1(rucksacks)
