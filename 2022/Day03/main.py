383356e7a1b9017fb7dd984d74a81c455d621fac
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-03 11:15:11 -0500

    Part 2 Complete Spin off priority summation into own method

--- 2022/Day03/main.py
+++ 2022/Day03/main.py
@@ -1,34 +1,64 @@
 #  Day 3
 import string
+from pprint import pprint
 
 def processInput(filename):
     with open(filename) as f:
         data = f.read().split('\n')
-    rucksacks = [ (x[:len(x)//2], x[len(x)//2:]) for x in data]
-    return rucksacks
+    return data
     
 filename = 'Day03/input.txt'
-rucksacks = processInput(filename)
+data = processInput(filename)
 priorityList = list(string.ascii_letters)
 
 
 # Part 1
-def part1(rucksacks):
+def part1(data):
+    rucksacks = [ (x[:len(x)//2], x[len(x)//2:]) for x in data]
     # Determine which letters are in common for each compartment of each rucksack
     lettersInCommon = []
     for r in rucksacks:
         lettersInCommon.append(findLetterInCommon(r))
     # print(lettersInCommon)
-
-    # Get priority of each letter and sum
-    prioritySum = 0
-    for letter in lettersInCommon:
-        prioritySum += priorityList.index(letter) + 1
-    print(f'The priority sum is {prioritySum}')
+    print(f'Part 1 Priority Sum: {getPrioritySum(lettersInCommon)}')
+   
 
 def findLetterInCommon(rucksack):
     for letter in rucksack[0]:
         if letter in rucksack[1]:
-            return letter    
+            return letter
 
-part1(rucksacks)
+def getPrioritySum(lettersInCommon):
+     # Get priority of each letter and sum
+    prioritySum = 0
+    for letter in lettersInCommon:
+        prioritySum += priorityList.index(letter) + 1
+    return prioritySum
+
+part1(data)
+
+# Part 2
+def part2(rucksacks):
+
+    # Split rucksacks into triplet groups
+    groups = []
+    for i in range(0, len(rucksacks), 3):
+        groups.append(rucksacks[i:i+3])
+    
+    # For each group, find the common letter
+    lettersInCommon = []
+    for group in groups:
+        lettersInCommon.append(findGroupLetter(group))
+    # print(lettersInCommon)
+
+    print(f'Part 2 Priority Sum: {getPrioritySum(lettersInCommon)}')
+
+
+def findGroupLetter(group):
+    # For each letter in first bag, see what is in common with bag 2
+    # If that letter is in bag 2, check bag 3
+    for letter in group[0]:
+        if letter in group[1] and letter in group[2]:
+            return letter
+
+part2(data)
