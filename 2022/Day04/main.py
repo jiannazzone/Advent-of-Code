e90249e4cc708fff57bbd9cecf96a089de59d0bd
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-04 13:53:49 -0500

    Part 1 complete

--- 2022/Day04/main.py
+++ 2022/Day04/main.py
@@ -11,8 +11,36 @@ def processInput(filename):
         for elf in thisPairRaw:
             thisPair.append([int(x) for x in elf])
         allPairs.append(thisPair)
-    print(data)
-    pprint(allPairs)
+    # print('All Pairs:')
+    # pprint(allPairs)
+    return allPairs
 
-filename = 'Day04/sample.txt'
-processInput(filename)
+filename = 'Day04/input.txt'
+allPairs = processInput(filename)
+
+def part1(allPairs):
+    # Create an extended list, showing all values for each elf
+    allPairsExtended = []
+    for pair in allPairs:
+        thisPair = []
+        for elf in pair:
+            thisPair.append(tuple(range(elf[0], elf[1]+1)))
+        allPairsExtended.append(thisPair)
+    # print('\nAll Pairs Extended')
+    # pprint(allPairsExtended)
+
+    overlapCount = 0
+    for pair in allPairsExtended:
+        if checkForCompleteOverlap(pair):
+            overlapCount += 1
+    print(f'There are {overlapCount} total overlaps.')
+
+def checkForCompleteOverlap(pair):
+    if all(x in pair[0] for x in pair[1]):
+        return True
+    elif all(x in pair[1] for x in pair[0]):
+        return True
+    else:
+        return False
+
+part1(allPairs)
