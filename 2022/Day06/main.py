9317a960fd19b4649a3ffaaf7f0e77402bf7c73a
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-06 07:35:28 -0500

    Consolidate everything into two methods

--- 2022/Day06/main.py
+++ 2022/Day06/main.py
@@ -3,39 +3,23 @@
 sample = open('Day06/sample.txt').read().splitlines()
 input = open('Day06/input.txt').read()
 
-def findStartMarker(packet):
-    for i in range(3, len(packet)):
-        bitSet = {packet[i-3], packet[i-2], packet[i-1], packet[i]}
-        if len(bitSet) == 4:
-            return i+1
-
-def findStartMessage(packet):
-    for i in range(13, len(packet)):
+def findUniqueString(packet, window):
+    for i in range(window-1, len(packet)):
         bitSet = set()
-        for j in range(0, 14):
+        for j in range(0, window):
             bitSet.add(packet[i-j])
-        if len(bitSet) == 14:
+        if len(bitSet) == window:
             return i+1
 
-def part1():
+def processStrings(window):
     # Test samples
-    print('Samples:')
+    sampleString = 'Samples: '
     for line in sample:
-        print(findStartMarker(line))
-    print('--------------\n')
+        sampleString += f'{findUniqueString(line, window)}, '
+    print(sampleString)
     
     # Use Input
-    print(f'Input: {findStartMarker(input)}')
-# part1()
-
-def part2():
-    # Test samples
-    print('Samples:')
-    for line in sample:
-        print(findStartMessage(line))
-    print('--------------\n')
-
-    # Use Input
-    print(f'Input: {findStartMessage(input)}')
-
-part2()
+    print(f'Input: {findUniqueString(input, window)}')
+    print()
+processStrings(4)
+processStrings(14)
