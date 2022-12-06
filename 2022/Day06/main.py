90700eac839116ee24d409c451683f2ac08e4d7d
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-06 07:23:36 -0500

    Part 1 complete

--- 2022/Day06/main.py
+++ 2022/Day06/main.py
@@ -1,1 +1,27 @@
 # Day 06
+
+sample = open('Day06/sample.txt').read().splitlines()
+input = open('Day06/input.txt').read()
+
+def findStartMarker(packet):
+    for i in range(3, len(packet)):
+        bitSet = set()
+        bitSet.add(packet[i-3])
+        bitSet.add(packet[i-2])
+        bitSet.add(packet[i-1])
+        bitSet.add(packet[i])
+        # print(bitSet)
+        if len(bitSet) == 4:
+            return i+1
+
+def part1():
+    # Test samples
+    print('Samples:')
+    for line in sample:
+        print(findStartMarker(line))
+    print('--------------\n')
+    
+    # Use Input
+    print(f'Input: {findStartMarker(input)}')
+
+part1()
