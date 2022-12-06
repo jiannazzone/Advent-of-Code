5b9bf5a11f97200d07bcde33f282f48d27c5e3ef
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-06 07:29:42 -0500

    Part 2 complete

--- 2022/Day06/main.py
+++ 2022/Day06/main.py
@@ -5,13 +5,16 @@ input = open('Day06/input.txt').read()
 
 def findStartMarker(packet):
     for i in range(3, len(packet)):
-        bitSet = set()
-        bitSet.add(packet[i-3])
-        bitSet.add(packet[i-2])
-        bitSet.add(packet[i-1])
-        bitSet.add(packet[i])
-        # print(bitSet)
+        bitSet = {packet[i-3], packet[i-2], packet[i-1], packet[i]}
         if len(bitSet) == 4:
+            return i+1
+
+def findStartMessage(packet):
+    for i in range(13, len(packet)):
+        bitSet = set()
+        for j in range(0, 14):
+            bitSet.add(packet[i-j])
+        if len(bitSet) == 14:
             return i+1
 
 def part1():
@@ -23,5 +26,16 @@ def part1():
     
     # Use Input
     print(f'Input: {findStartMarker(input)}')
+# part1()
 
-part1()
+def part2():
+    # Test samples
+    print('Samples:')
+    for line in sample:
+        print(findStartMessage(line))
+    print('--------------\n')
+
+    # Use Input
+    print(f'Input: {findStartMessage(input)}')
+
+part2()
