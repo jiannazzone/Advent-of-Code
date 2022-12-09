2618456dc9674a89107f698da1496bb86005694a
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-12-09 09:44:07 -0500

    Day 8 Part 2 complete

--- 2022/Day08/main.py
+++ 2022/Day08/main.py
@@ -8,6 +8,7 @@ def processInput(filename):
 
 treeMap = processInput('Day08/input.txt')
 
+# Part 1
 def findVisible():
     # All trees on the edge are visible
     # But we need to be careful not to double-count the corners
@@ -15,20 +16,11 @@ def findVisible():
     visibleTrees = edgeCount
 
     # Find the max values in each row and each column
-    rowMax = []
-    for row in treeMap:
-        rowMax.append(max(row))
-    
-    colMax = []
-    for i in range(len(treeMap[0])):
-        thisCol = [x[i] for x in treeMap]
-        colMax.append(max(thisCol))
-
     for row in range(1,len(treeMap)-1):
         for col in range(1,len(treeMap[0])-1):
             thisTree = treeMap[row][col]
             visibleTrees += checkTree(thisTree, row, col)
-    print(visibleTrees)
+    print(f'There are {visibleTrees} visible trees.')
 
 def checkTree(thisTree, row, col):
     # Check the row
@@ -46,3 +38,69 @@ def checkTree(thisTree, row, col):
     return 0
 
 findVisible()
+
+# Part 2
+def findScenic():
+    maxScore = 0
+    # We can skip the edge trees, since they will always have a scenic score of 0
+
+    for row in range(1,len(treeMap)-1):
+        for col in range(1,len(treeMap[0])-1):
+            thisTree = treeMap[row][col]
+            thisScore = checkScore(thisTree, row, col)
+            if thisScore > maxScore:
+                maxScore = thisScore
+    print(f'The max possible score is {maxScore}.')
+
+def checkScore(thisTree, row, col):
+    left = treeMap[row][:col]
+    right = treeMap[row][col+1:]
+    up = [x[col] for x in treeMap[:row]]
+    down = [x[col] for x in treeMap[row+1:]]
+    
+    viewCounts = {
+        'up': 0,
+        'down': 0,
+        'left': 0,
+        'right': 0
+    }
+
+    # Check Up
+    for tree in reversed(up):
+        if tree < thisTree:
+            viewCounts['up'] += 1
+        else:
+            viewCounts['up'] += 1
+            break
+    
+    # Check Down
+    for tree in down:
+        if tree < thisTree:
+            viewCounts['down'] += 1
+        else:
+            viewCounts['down'] += 1
+            break
+    
+    # Check Left
+    for tree in reversed(left):
+        if tree < thisTree:
+            viewCounts['left'] += 1
+        else:
+            viewCounts['left'] += 1
+            break
+    
+    # Check Right
+    for tree in right:
+        if tree < thisTree:
+            viewCounts['right'] += 1
+        else:
+            viewCounts['right'] += 1
+            break
+    
+    # print(viewCounts)
+    score = 1
+    for val in viewCounts.values():
+        score *= val
+    return score
+
+findScenic()
