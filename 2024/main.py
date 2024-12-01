a9e310a39c1d127e2f0492b289590e80d7e8914a
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-12-01 12:07:21 -0500

    Add comments

--- 2024/main.py
+++ 2024/main.py
@@ -23,17 +23,23 @@ list1, list2 = parseInput(inputPath)
 # Part 1
 list1.sort()
 list2.sort()
-totalDifference = 0
+differenceScore = 0
 
+# Get difference between each element in ordered lists, then add that difference to the total
 for i in range(len(list1)):
-    totalDifference += abs(list1[i] - list2[i])
+    differenceScore += abs(list1[i] - list2[i])
 
-print(f'Difference score: {totalDifference}')
+print(f'Difference score: {differenceScore}')
 
 
 # Part 2
 similarityScore = 0
+
+# Get a frequency count for each element in list2
 list2Count = collections.Counter(list2)
+
+# Multiply each element in list1 by its frequency in list2
+# Skip elements that do not appear in list2
 for x in list1:
     if x in list2Count.keys():
         similarityScore += x * list2Count[x]
